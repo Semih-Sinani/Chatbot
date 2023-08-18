23412b881b2a7960bf295f86cdabd3534d7ee106
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-08-18 19:33:39 +0200

    Completed

--- chat.py
+++ chat.py
@@ -30,7 +30,7 @@ bot_name = "Sam"
 def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0]
+    X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
     output = model(X)
