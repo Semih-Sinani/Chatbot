b6d760f53564407c404e6546d6b02bd6ffa861f2
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-08-18 19:30:29 +0200

    Modified file

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ with open('intents.json', 'r') as f:
 
 all_words = []
 tags = []
-xy = []
+x = []
 # loop through each sentence in our intents patterns
 for intent in intents['intents']:
     tag = intent['tag']
