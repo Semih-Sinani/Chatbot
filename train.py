63d3ae7139ea47757be510e4594ad1a98e27680a
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-08-18 19:31:15 +0200

    modified file

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
 with open('intents.json', 'r') as f:
-    intents = json.load(f)
+    intents = json.load()
 
 all_words = []
 tags = []
